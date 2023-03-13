0ac656d34c1aca568e2cc18a71b5e5288cfb2a37
codingminsik <codingminsik@gmail.com>
2023-03-13 14:09:12 +0900

    응용

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,14 +1,43 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 3; i++) {
-			for (int j = 1; j < 6; j++) {
-				for (int a = 1; a<= 1; a++) {
-				System.out.print("*");
-				}
+		Scanner sc= new Scanner(System.in);
+		System.out.println("===프로그램 시작===");
+		int id = 1;
+		
+		while (true) {
+			System.out.print("명령어 ) ");
+			String command = sc.nextLine();
+			
+			if (command.equals("post write")) {
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				id++;
 			}
-			System.out.println("*");
+			
+			if (command.equals("post list")) {
+				System.out.println("게시글이 없습니다.");
+//				System.out.println("번호   ||   제목");
+			}
+			
+			if (command.equals("exit")){
+				break;
+			}
 		}
+		System.out.println("===프로그램 종료===");
+		sc.close();
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
 }
