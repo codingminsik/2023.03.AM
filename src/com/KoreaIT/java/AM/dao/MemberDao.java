f9b3685de6bf6f10dd08c791e0c4f276d665b2bb
codingminsik <codingminsik@gmail.com>
2023-03-16 14:45:28 +0900

    자바게시판

--- src/com/KoreaIT/java/AM/dao/MemberDao.java
+++ src/com/KoreaIT/java/AM/dao/MemberDao.java
@@ -24,4 +24,48 @@ public class MemberDao extends Dao {
 	public int setNewId() {
 		return lastId + 1;
 	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return null;
+		}
+
+		return members.get(index);
+	}
+
+	public boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
+	public String getMemberNameById(int id) {
+		for (Member member : members) {
+			if (id == member.id) {
+				return member.name;
+			}
+		}
+		return null;
+	}
 }
